90ee5baa3b66da0bc612d7c4bd342fb38936db47
Regis Digital <regissdigital@gmail.com>
2026-06-11 23:14:50 +0530

    Feat: Create CallPilot Credit Ledger Doctype and balance logic

---
+++ callpilot_ai/callpilot_ai/doctype/callpilot_credit_ledger/callpilot_credit_ledger.py
@@ -0,0 +1,15 @@
+﻿import frappe
+from frappe.model.document import Document
+
+class CallPilotCreditLedger(Document):
+    pass
+
+@frappe.whitelist()
+def get_client_balance(client):
+    credits = frappe.db.sql("SELECT SUM(amount) FROM 	abCallPilot Credit Ledger WHERE client=%s AND transaction_type='Credit'", client)
+    debits = frappe.db.sql("SELECT SUM(amount) FROM 	abCallPilot Credit Ledger WHERE client=%s AND transaction_type='Debit'", client)
+    
+    total_credit = credits[0][0] if credits and credits[0][0] else 0
+    total_debit = debits[0][0] if debits and debits[0][0] else 0
+    
+    return total_credit - total_debit
